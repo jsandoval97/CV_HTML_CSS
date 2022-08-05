db3eeed354c258134bd952b3de8f88fb39242445
jsandoval97 <88573821+jsandoval97@users.noreply.github.com>
2022-08-04 21:43:58 -0300

    Commit Inicial

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("nav-menu_visible")) {
+        navToggle.setAttribute("aria-label", "Cerrar menú");
+      } else {
+        navToggle.setAttribute("aria-label", "Abrir menú");
+      }
+});*/
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
