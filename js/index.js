179a919ba28709aa38f4916cde9def121f520903
jsandoval97 <88573821+jsandoval97@users.noreply.github.com>
2022-08-15 20:34:02 -0300

    Contact Update
    Agregue formulario de contacto funcional

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+//Hace que se marque en la barra de navegación en que sección me encuentro
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const id = entry.target.getAttribute("id");
+    const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+
+    if (entry.isIntersecting) {
+      menuLink.classList.add("selected");
+    } else {
+      menuLink.classList.remove("selected");
+    }
+  })
+  //agrego margen para que no seleccione mas de una sección al mismo tiempo
+}, {rootMargin: "-50% 0px -50% 0px"})
+
+//Hace que el menú se cierre cuando selecciono una sección
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener("click", function() {
+    menu.classList.remove("menu_opened");
+  })
+
+  //aplico el observer
+  const hash = menuLink.getAttribute("href");
+  const target = document.querySelector(hash);
+  if(target) {
+    observer.observe(target);
+  }
+})
